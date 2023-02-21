a019821a2a22a67f9ac89a346f71c78f2ab84aa2
KarimCarl <cndungu109@gmail.com>
2023-02-21 14:59:12 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+     # setter Method
+     def name=(name)
+        @name = name
+    end
 
+    # getter Method
+    def name
+        @name
+    end
+
+    # setter Method
+    def job=(job)
+        @job="Singer"
+    end
+
+    # getter Method
+    def job
+        @job
+    end
 end
